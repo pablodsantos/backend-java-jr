7b6b78414db0e2ec7f34c20d49d24cc4e52b7b6e
Pablo dos Santos Garajau <pablo.dsantosg@gmail.com>
2022-04-18 21:29:00 -0300

    Add files via upload

--- Program.java
+++ Program.java
@@ -1,81 +1,67 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
-
-import entities.Employe;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
-		
 		Scanner sc = new Scanner(System.in);
 		
-		Integer id;
-		String name;
-		Double salary;
+		System.out.print("Digite ordem da matriz: ");
+		int n = sc.nextInt();
 		
-		System.out.println("How many employes will to registered? ");
-		int quantityEmployes = sc.nextInt();
-		
-		List <Employe> listEmployes = new ArrayList<>();
-		
-		for (int i = 0; i < quantityEmployes; i++) {
-			System.out.println("\nEmploye #" + (i+1));
-			System.out.print("Id: ");
-			id = sc.nextInt();
-			
-			for(int j = 0; j <listEmployes.size(); j++) {
-				while(listEmployes.get(j).getId() == id) {
-					System.out.println("Id exists! Please another value.");
-					System.out.print("Id: ");
-					id = sc.nextInt();
-				}
+		int[][] mat = new int[n][n];
+				
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				mat[i][j] = sc.nextInt();
 			}
-			
-			System.out.print("Name: ");
-			name = sc.next();
-			System.out.print("Salary: ");
-			salary = sc.nextDouble();
-			
-			Employe employe = new Employe(id, name, salary);
-			listEmployes.add(employe);
 		}
 		
-		System.out.println("\nList of employes: ");
-		for(Employe emp : listEmployes) {
-			System.out.println(emp);
-		}
+		System.out.println("Matriz completa: ");
+		imprimeMatrizOrdemN(mat, n);
 		
-		System.out.print("Enter the employe id that will have salary increase: ");
-		Integer idIncrease = sc.nextInt();
+		System.out.println("Diagonal principal: ");
+		diagonalPrincipal(mat, n);
+		
+		System.out.println();
+		System.out.println("Quantidade numeros negativos: ");
+		System.out.println(quantidadeNumerosNegativos(mat, n));
 		
 		sc.close();
-				
-		for(int i = 0; i < listEmployes.size(); i++) {
-			int count = 0;
-			if(listEmployes.get(i).getId() == idIncrease) {
-				System.out.println("Id encontrado!");
-				System.out.print("Enter to percentage: ");
-				double percentage = sc.nextDouble();
-				listEmployes.get(i).addSalary(percentage);
-				break;
-			}else {
-				count++;
+	}
+	
+	public static void imprimeMatrizOrdemN(int[][] mat, int n) {
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				System.out.print(mat[i][j] + "\t");
 			}
-			
-			if(count == listEmployes.size() - 1) {
-				System.out.println("Id not find!");
-				return;
+			System.out.println();
+		}
+	}
+	
+	public static void diagonalPrincipal(int[][] mat, int n) {
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(i == j) {
+					System.out.print(mat[i][j] + "\t");
+				}
+			}
+		}
+	}
+	
+	public static int quantidadeNumerosNegativos(int mat[][], int n) {
+		int count = 0;
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(mat[i][j] < 0) {
+					count++;
+				}
 			}
 		}
 		
-		System.out.println("\nList of employes update: ");
-		for(Employe emp : listEmployes) {
-			System.out.println(emp);
-		}
+		return count;
 	}
 }
--- Program2.java
+++ Program2.java
@@ -1,44 +1,85 @@
 package application;
 
+import java.util.Locale;
 import java.util.Scanner;
-
-import entities.Estudante;
 
 public class Program2 {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
 		Scanner sc = new Scanner(System.in);
-		int numeroMaximoDeQuartos = 10;
-		Estudante[] estudantes = new Estudante[numeroMaximoDeQuartos];
 		
-		System.out.println("Disponiveis[10] - Numero de quartos:");
-		int quantidadeDeQuartos = sc.nextInt();
+		System.out.print("Linhas: ");
+		int n = sc.nextInt();
+		System.out.print("Colunas: ");
+		int m = sc.nextInt();
 		
-		for(int i=0; i<quantidadeDeQuartos; i++) {
-			sc.nextLine();
-			System.out.println("Nome: ");
-			String nome = sc.nextLine();
-			System.out.println("E-mail: ");
-			String email = sc.nextLine();
-			int numeroQuarto;
-			do {
-				System.out.println("Numero do quarto: ");
-				numeroQuarto = sc.nextInt();
-			}while(numeroQuarto > numeroMaximoDeQuartos);
-						
-			estudantes[numeroQuarto] = new Estudante(nome,email);
+		int[][] mat = new int[n][m];
+		
+		for (int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				mat[i][j] = sc.nextInt();
+			}
 		}
 		
-		imprimeVetorDeAlunos(estudantes, numeroMaximoDeQuartos);
+		System.out.print("Number: ");
+		int num = sc.nextInt();
+		
+		System.out.println("Matriz completa: ");
+		imprimeMatriz(mat, n, m);
+		
+		if(existsNumberMatriz(mat, n, m, num) > 0) {
+			System.out.println("Encontrado!");
+			imprimePosicoes(mat, n, m, num);
+		}else {
+			System.out.println("Não encontrado!");
+		}
+		
 		sc.close();
 	}
 	
-	private static void imprimeVetorDeAlunos(Estudante[] estudantes, int numeroMaximoDeQuartos) {
-		for(int i=0; i<numeroMaximoDeQuartos; i++) {
-			if(estudantes[i] != null) {
-				System.out.println("Nome: " + estudantes[i].getName()
-				+ " | E-mail: " + estudantes[i].getEmail()
-				+ " | Numero do quarto: " + i);
+	public static int existsNumberMatriz(int[][] mat, int n, int m, int number) {
+		int count = 0;
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				if(mat[i][j] == number) {
+					count ++;
+					break;
+				}
+			}
+		}
+		
+		return count;
+	}
+	
+	public static void imprimeMatriz(int[][] mat, int n, int m) {
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				System.out.print(mat[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void imprimePosicoes(int[][] mat, int n, int m, int number) {
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				if(mat[i][j] == number) {
+					System.out.println(i + " , " + j + "\n");
+					if(j != 0) {
+						System.out.println("Esquerda: " + mat[i][j - 1]);
+					}
+					if(j < m -1) {
+						System.out.println("Direita: " + mat[i][j + 1]);
+					}
+					if(i != 0) {
+						System.out.println("Acima: " + mat[i - 1][j]);
+					}
+					if(i != n - 1) {
+						System.out.println("Abaixo: " + mat[i + 1][j]);
+					}
+				}
 			}
 		}
 	}
